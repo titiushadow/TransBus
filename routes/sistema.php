41ef1d5df188f9a667c2bc23d03bf7a939db7bae
titiushadow <mesaque1212@gmail.com>
2023-08-01 17:17:58 -0300

    Mais algumas correções (Ainda não funciona)

--- routes/sistema.php
+++ routes/sistema.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SistemaController;
+use App\Http\Controllers\CadAlunoController;
 
 Route::group([], function () {
     Route::get('/cad-alunos', [SistemaController::class, 'CadAlunos'])->name('CadAlunos');
     Route::get('/show_alunos', [SistemaController::class, 'ShowAlunos'])->name('ShowAlunos');
-    Route::post('/cad_alunos', [SistemaController::class, 'store'])->name('store_aluno');
+    Route::Post('/cad_alunos', [CadAlunoController::class, 'store'])->name('store_aluno');
 });
 ?>
